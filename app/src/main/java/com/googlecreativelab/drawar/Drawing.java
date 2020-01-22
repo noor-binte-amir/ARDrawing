3f319c2bde6547ecbd627a413d211bea156782f9
Noor Binte Amir <noor.binte.amir@gmail.com>
2020-01-22 20:17:36 +0500

    Upload

---
+++ app/src/main/java/com/googlecreativelab/drawar/Drawing.java
@@ -0,0 +1,14 @@
+package com.googlecreativelab.drawar;
+
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
+
+import javax.vecmath.Vector2f;
+import javax.vecmath.Vector3f;
+
+public class Drawing {
+
+    public ArrayList<ArrayList<Vector3f>> strokes = null;
+    public Vector2f position = null;
+}
